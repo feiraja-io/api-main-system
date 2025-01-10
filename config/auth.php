968de76b6da7d11551f782ac4516cb6b29330ebc
CandidoRPNeto <candido.neto@ucsal.edu.br>
2025-01-10 10:48:49 -0300

    Create login

--- config/auth.php
+++ config/auth.php
@@ -15,7 +15,7 @@ return [
 
     'defaults' => [
         'guard' => env('AUTH_GUARD', 'web'),
-        'passwords' => env('AUTH_PASSWORD_BROKER', 'stores'),
+        'passwords' => env('AUTH_PASSWORD_BROKER', 'users'),
     ],
 
     /*
@@ -38,7 +38,7 @@ return [
     'guards' => [
         'web' => [
             'driver' => 'session',
-            'provider' => 'stores',
+            'provider' => 'users',
         ],
     ],
 
@@ -60,15 +60,10 @@ return [
     */
 
     'providers' => [
-        'stores' => [
-            'driver' => 'eloquent',
-            'model' => env('AUTH_MODEL', App\Models\Store::class),
+        'users' => [
+            'driver' => 'database',
+            'table' => 'users',
         ],
-
-        // 'users' => [
-        //     'driver' => 'database',
-        //     'table' => 'users',
-        // ],
     ],
 
     /*
@@ -91,8 +86,8 @@ return [
     */
 
     'passwords' => [
-        'stores' => [
-            'provider' => 'stores',
+        'users' => [
+            'provider' => 'users',
             'table' => env('AUTH_PASSWORD_RESET_TOKEN_TABLE', 'password_reset_tokens'),
             'expire' => 60,
             'throttle' => 60,
--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -13,11 +14,18 @@ class LoginTest extends TestCase
      */
     public function test_example(): void
     {
-        // $response = $this->post('/login', [
-        //     'email' => "teste@teste.com",
-        //     'password' => "12345678",
-        // ]);
+        $u = [
+            "name" => "Teste",
+            'email' => "test@teste.com",
+            'password' => "12345678",
+        ];
+        $user = User::create($u);
+        $response = $this->post('/login', [
+            'email' => $u['email'],
+            'password' => $u['password']
+        ]);
 
-        // $response->assertStatus(200);
+        $user->delete();
+        $response->assertStatus(200);
     }
 }
